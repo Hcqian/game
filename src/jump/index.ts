3863b14de87ca5880c564963591c76f2a77baf5c
haocq <517571978@qq.com>
2023-10-08 16:30:06 +0800

    modify jump

--- src/jump/index.ts
+++ src/jump/index.ts
@@ -15,14 +15,16 @@ class loadGame extends Phaser.Scene{
         this.load.audio('false1','jump/assets/false1.mp3');
         this.load.audio('false2','jump/assets/false2.mp3');
         this.load.audio('click','jump/assets/click.mp3');
-        this.load.spritesheet('player','jump/assets/bird.png',{frameWidth:34,frameHeight:24})
+        this.load.image('player', 'jump/assets/doodler.png');
+       // this.load.spritesheet()
+       // this.load.spritesheet('player','jump/assets/bird.png',{frameWidth:34,frameHeight:24})
     }
 
     create(){
         let bg = this.add.tileSprite(this.cameras.main.width/2,this.cameras.main.height/2,this.cameras.main.width,this.cameras.main.height,'backgroud')
         let startbutton = this.add.image(this.cameras.main.width/2,this.cameras.main.height-150,'atlas','Play_01').setInteractive()
-        this.add.image(320,50,'atlas','top')  //怎么遮盖
-        this.add.image(320,this.cameras.main.height,'atlas','bottom')
+
+
 
         startbutton.on('pointerover',()=>{
             startbutton.setTexture('atlas','Play_02')
@@ -38,17 +40,61 @@ class loadGame extends Phaser.Scene{
     }
 
 }
+
+class butEnemy{
+    enemyBut?:Phaser.Types.Physics.Arcade.SpriteWithStaticBody
+    butAnims?:Phaser.Animations.Animation
+    butCollider?:Phaser.Physics.Arcade.Collider
+    scene:startGame
+    // x:number
+    // y:number
+
+    constructor(config:{x:number,y:number,scene:startGame}) {
+        this.scene =config.scene
+        this.genBut(config.x,config.y)
+    }
+    private genBut(x:number,y:number){
+        this.enemyBut = this.scene.physics.add.staticSprite(x,y,'atlas','but1')
+        this.scene.anims.create({
+            key: 'butFly',
+            frames:this.scene.anims.generateFrameNames('atlas',{
+                prefix:'but',
+                start:1,
+                end:4
+                //zeroPad:1,
+                //suffix:'.png'
+            }),
+            frameRate:50,
+            repeat:-1
+        })
+        this.enemyBut.anims.play('butFly')
+        this.butCollider = this.scene.physics.add.collider(this.scene.player!,this.enemyBut,()=>{
+            this.scene.gameOver()
+        })
+        this.scene.platforms!.add(this.enemyBut)
+    }
+    removeBut(){
+        this.scene.platforms!.remove(this.enemyBut!)
+        this.scene.anims.remove('butFly')
+        this.scene.physics.world.removeCollider(this.butCollider!)
+        this.enemyBut?.destroy()
+
+    }
+
+
+}
 class startGame extends Phaser.Scene{
 
-    player?:Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
+    public player?:Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
     platforms?:Phaser.Physics.Arcade.StaticGroup
     cursors?:Phaser.Types.Input.Keyboard.CursorKeys
     jumped?:boolean =false
     move?:boolean = false
     backgroud?:Phaser.GameObjects.TileSprite
     platformsColloder?:Phaser.Physics.Arcade.Collider
+    enemyBut?:butEnemy|undefined
 
-    playerY:number = 0
+//    playerY:number = 0
 
     over:boolean = false
 
@@ -58,9 +104,16 @@ class startGame extends Phaser.Scene{
 
     create(data: { backgroud:Phaser.GameObjects.TileSprite }){
         this.backgroud = data.backgroud
-        this.player = this.physics.add.sprite(this.cameras.main.width/2,this.cameras.main.height-200,'player')
+        this.player = this.physics.add.sprite(this.cameras.main.width/2,this.cameras.main.height-200,'player').setScale(0.5,0.5)
         this.player.setCollideWorldBounds(true)
-        this.playerY = this.player.y
+
+     //   this.playerY = this.player.y
+        // this.player.setScrollFactor(0,1)//相机
+        //let camera = this.cameras.main.setSize(500,400)
+        //this.cameras.main.startFollow(this.player)
+
+       // this.cameras.main.stopFollow()
+
         this.jumped = false;
         this.platforms = this.physics.add.staticGroup()
         this.platforms.create(this.cameras.main.width/2,this.cameras.main.height-150,'atlas','platform0');
@@ -68,14 +121,25 @@ class startGame extends Phaser.Scene{
         this.platformsColloder = this.physics.add.collider(this.player,this.platforms)
 
         this.cursors = this.input.keyboard!.createCursorKeys();
+        let top = this.add.image(320,50,'atlas','top').setDepth(99)
+        let bottom = this.add.image(320,this.cameras.main.height,'atlas','bottom').setDepth(100)
+
         this.addPlatform()
+        //this.genBut(200,200)
+        //this.enemyBut = new butEnemy({x:200,y:200,scene:this})
+        //const object2 = this.add.container(0, 0, [top,bottom]);
+        //this.cameras.main.startFollow(object2)
+      //  this.children.bringToTop(top)
+      //  this.children.bringToTop(bottom)
+        console.log(this.children)
 
     }
+
 
     private addPlatform(){
         let  width = Phaser.Math.Between(100,400)
         let  height = Phaser.Math.Between(200,300)
-        this.platforms!.create(width,0,'atlas','platform0')
+        this.platforms!.create(width,100,'atlas','platform0')
         width = Phaser.Math.Between(100,400)
         this.platforms!.create(width,height,'atlas','platform0')
         width = Phaser.Math.Between(100,400)
@@ -94,25 +158,37 @@ class startGame extends Phaser.Scene{
             if (body.y > 800) {
                 let width = Phaser.Math.Between(100,400)
                 body.reset(width,0)
+                if (this.enemyBut == undefined) this.randomBut(Phaser.Math.Between(100,400),-100)
+            }
+            if (this.enemyBut != undefined && this.enemyBut.enemyBut!.y > 800) {
+                this.enemyBut?.removeBut()
+                this.enemyBut =undefined
             }
             return true
         })
-
+    }
+    randomBut(x:number,y:number){
+        if (Phaser.Math.Between(0, 19) < 2)
+        {
+            this.enemyBut = new butEnemy({x:x,y:y,scene:this})
+        }
+    }
+    gameOver(){
+        this.over =true
+        this.add.image(this.cameras.main.width/2,this.cameras.main.height/2,'atlas','gameOver')
     }
 
     update(time: number, delta: number) {
         if (this.over) return
-        if (this.player!.y > 750) {this.over =true}
+        if (this.player!.y > 750) this.gameOver()
         super.update(time, delta);
         this.updatePlatform()
-       // if (!this.move && this.cursors!.left.isDown) {
         if (this.cursors!.left.isDown) {
-         //   this.move =true
             this.player!.setVelocityX(-160);
-       // }else if(!this.move && this.cursors!.right.isDown){
+            this.player!.flipX =true
        }else if(this.cursors!.right.isDown){
-           // this.move =true
             this.player!.setVelocityX(160);
+            this.player!.flipX =false
         }else {
             // if(this.player!.body.touching.down) {
             //  //   this.move =false
@@ -125,17 +201,18 @@ class startGame extends Phaser.Scene{
             this.player!.setVelocityX(0)
         }
         if(!this.jumped){
+           // console.log(Math.floor(Math.random() * 10))
             this.jumped = true
-            this.player!.setVelocityY(-400)
+            this.player!.setVelocityY(-450)
             this.platformsColloder!.active = false
         }else {
          //   console.log(this.player!.body.velocity.y)
-            if (!this.platformsColloder!.active && this.player!.body.velocity.y > 0) {
+            if (!this.platformsColloder!.active && this.player!.body.velocity.y > 0) {//跳跃过程中，防止上方触碰平台
                 this.platformsColloder!.active = true
             }
         }
-
     }
+
 }
 
 
